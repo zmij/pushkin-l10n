d4e536ab4cbaae82f0b68a0f95d749f28aa7b485
zmij <sergei.a.fedorov@gmail.com>
2016-10-23 13:49:56 +0300

    Move cereal::json aside to make the lib pure

---
+++ include/pushkin/l10n/message.hpp
@@ -0,0 +1,156 @@
+/**
+ * message.hpp
+ *
+ *  Created on: 11 окт. 2015 г.
+ *      Author: zmij
+ */
+
+#ifndef PUSHKIN_L10N_MESSAGE_HPP_
+#define PUSHKIN_L10N_MESSAGE_HPP_
+
+#include <string>
+#include <memory>
+#include <iosfwd>
+
+#include <boost/optional.hpp>
+#include <boost/variant.hpp>
+#include <boost/locale.hpp>
+
+namespace psst {
+namespace l10n {
+
+class format_args;
+
+class locale_name_facet : public std::locale::facet {
+public:
+    static std::locale::id id;
+public:
+    locale_name_facet(std::string const& n) : facet(), name_(n) {}
+    virtual ~locale_name_facet() {}
+
+    std::string const&
+    name() const
+    { return name_; }
+private:
+    std::string name_;
+};
+
+/**
+ * Class for representing a message that must be translated when sending
+ * to output. The message can be read in runtime from text files and used
+ * to generate gettext po files.
+ */
+class message {
+public:
+    typedef boost::optional< std::string >      optional_string;
+    typedef std::unique_ptr< format_args >      optional_format_args;
+    typedef ::std::string::size_type            size_type;
+    typedef boost::locale::message              localized_message;
+    typedef boost::locale::format               formatted_message;
+
+    enum type {
+        EMPTY,
+        SIMPLE,
+        PLURAL,
+        CONTEXT,
+        CONTEXT_PLURAL
+    };
+public:
+    /**
+     * Construct a default empty message
+     */
+    message();
+    message(optional_string const& domain);
+    /**
+     * Construct a message with message id
+     */
+    message(std::string const& id,
+            optional_string const& domain = optional_string());
+    /**
+     * Construct a message with message id and a context
+     */
+    message(std::string const& context,
+            std::string const& id,
+            optional_string const& domain = optional_string());
+    /**
+     * Construct a message with singular/plural
+     */
+    message(std::string const& singular,
+            std::string const& plural,
+            int n,
+            optional_string const& domain = optional_string());
+    /**
+     * Construct a message with singular/plural and a context
+     */
+    message(std::string const& context,
+            std::string const& singular,
+            std::string const& plural,
+            int n,
+            optional_string const& domain = optional_string());
+
+    message(message const&);
+    message(message&&) = default;
+
+    void
+    swap(message& rhs) noexcept;
+
+    bool
+    empty() const
+    { return type_ == EMPTY || id_.empty(); }
+
+    bool
+    has_context() const
+    { return context_.is_initialized(); }
+    bool
+    has_plural() const
+    { return plural_.is_initialized(); }
+    bool
+    has_format_args() const
+    { return format_args_.get() != nullptr; }
+
+    void
+    set_domain( std::string const& );
+
+    void
+    write(std::ostream& os) const;
+
+    localized_message
+    translate() const;
+private:
+    type                        type_;
+    std::string                 id_;
+    optional_string             context_;
+    optional_string             plural_;
+    optional_string             domain_;
+
+    int                         n_;
+    optional_format_args        format_args_;
+};
+
+std::ostream&
+operator << (std::ostream& os, message const& v);
+
+class format_args {
+public:
+    typedef boost::variant<
+            int64_t, double, bool,
+            std::string, message >              arg_type;
+    typedef std::vector< arg_type >             arguments_vector;
+    typedef boost::locale::format               formatted_message;
+    typedef std::vector< std::string >          format_buffers_type;
+    typedef boost::optional< std::string >      optional_string;
+public:
+    size_t
+    size() const
+    { return arguments_.size(); }
+    void
+    feed_arguments(formatted_message& msg, format_buffers_type&,
+            std::locale const&, optional_string const& domain) const;
+private:
+    arguments_vector arguments_;
+};
+
+} /* namespace l10n */
+} /* namespace psst */
+
+#endif /* PUSHKIN_L10N_MESSAGE_HPP_ */
